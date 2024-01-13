81c2cba8d95a1986215db58242be8be5c6f221cc
Sushantfox <sushant091.hitcsecs2020@gmail.com>
2024-01-13 17:44:26 +0530

    first commit

---
+++ backend/sql_connection.py
@@ -0,0 +1,15 @@
+import datetime
+import mysql.connector
+
+__cnx = None
+
+def get_sql_connection():
+  print("Opening mysql connection")
+  global __cnx
+
+  if __cnx is None:
+    __cnx = mysql.connector.connect(user='sush', password='123sk456', host='127.0.0.1', database='grocery_store')
+
+  return __cnx
+
+ 
---
+++ sql.sql
@@ -0,0 +1,1 @@
+SELECT * FROM grocery_store.products;
